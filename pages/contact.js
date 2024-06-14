fa48433d858e3b914ffa37de2de18af1a564926d
Nick Kocharhook <nick@kocharhook.com>
2024-06-14 22:15:15 +0100

    Flesh out contact page.

--- pages/contact.js
+++ pages/contact.js
@@ -9,12 +9,16 @@ import Link from '../src/Link';
 export default function About() {
     return (
         <Container maxWidth="sm">
-            <Box sx={{ my: 4 }}>
+            <Box sx={{ py: 3 }}>
                 <Typography variant="h4" component="h1" gutterBottom>
-                    Material UI - Next.js example
+                    Get in touch
                 </Typography>
-                <Button variant="contained" component={Link} noLinkStyle href="/">
-                    Go to the main page
+                <Para>
+                    I want this project to be a useful resource for the whole US genealogy community. If you have thoughts on how this data could be used better, please get in touch! If you have technical skills, I’d love to hear from you. And if you’ve found a bug, please open an issue on the project’s <Link href="https://github.com/nk9/gannett">GitHub page</Link>.
+                </Para>
+                <Button variant="contained" component={Link} noLinkStyle href="mailto:quaver.drake.0s@icloud.com"
+                    sx={{ mt: 3 }}>
+                    Send Email
                 </Button>
             </Box>
         </Container>
