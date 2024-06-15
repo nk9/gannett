2a34195da554e468fb3d44eb85ae772fe4671741
Nick Kocharhook <nick@kocharhook.com>
2024-06-16 00:26:24 +0100

    Fix Census Links list at various window sizes.

--- components/InfoPanel.js
+++ components/InfoPanel.js
@@ -1,19 +1,21 @@
 import { useState } from 'react';
 
-import { resourceFormats, zoomThreshold } from "@/constants";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import LinkIcon from '@mui/icons-material/Link';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { Tooltip } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import { sprintf } from 'sprintf-js';
+
+import { resourceFormats, zoomThreshold } from "@/constants";
 import District from 'src/District';
 import Link from 'src/Link';
 import styles from "./InfoPanel.module.scss";
-import useMapState from '/stores/mapStore';
-
 import Para from './Para';
+import useMapState from '/stores/mapStore';
 
 export default function InfoPanel({ metroInfo, bottom }) {
     const currentZoomLevel = useMapState('currentZoomLevel')
@@ -59,9 +61,10 @@ export default function InfoPanel({ metroInfo, bottom }) {
                         pay = " ($)";
                     }
                     return (
-                        <li key={index}><Link href={href} target="_blank">{form.title + pay}</Link>
+                        <ListItem key={index} sx={{ pl: .5, pb: 0, pt: 0, display: "list-item" }}>
+                            <Link href={href} target="_blank">{form.title + pay}</Link>
                             <OpenInNewIcon fontSize="xsmall" sx={{ position: "relative", bottom: "-3px", left: "3px" }} />
-                        </li>);
+                        </ListItem>);
                 })
             }
 
@@ -117,13 +120,13 @@ export default function InfoPanel({ metroInfo, bottom }) {
                 <Typography variant='h6' sx={{
                     ml: { xs: 2, sm: 0 }
                 }}>Census pages</Typography>
-                <ul>
+                <List sx={{ listStyle: "disc", pl: { xs: 6, sm: 4 } }}>
                     {census_links}
-                </ul>
+                </List>
                 <Typography variant='h6' sx={{
                     ml: { xs: 2, sm: 0 }
                 }}>Something wrong?</Typography>
-                <Para sx={{ ml: { xs: 1, sm: 0 } }}> If you see any problems with this Enumeration District, <Link href="/contact">please get in touch</Link>. Include a clear explanation of what's wrong and a link to the ED.
+                <Para sx={{ ml: { xs: 2, sm: 0 } }}> If you see any problems with this Enumeration District, <Link href="/contact">please get in touch</Link>. Include a clear explanation of what's wrong and a link to the ED.
                 </Para>
             </>);
 
