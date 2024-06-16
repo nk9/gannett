28e459d0ee656c77f951c51e37b36684bcfa49d4
Nick Kocharhook <nick@kocharhook.com>
2024-06-16 14:57:05 +0100

    Pass props through Para to Typography. Fix className, which can't actually be bare.

--- components/Para.js
+++ components/Para.js
@@ -1,12 +1,14 @@
 import Typography from '@mui/material/Typography';
 import { styled } from "@mui/system";
  
-function Para({ className, children }) {
+function Para({ className, children, ...props }) {
     return (
         <Typography
             variant='body1'
-            className
-            sx={{ mt: 1 }}>
+            className={className}
+            sx={{ mt: 2 }}
+            {...props}
+        >
             {children}
         </Typography>
     )
